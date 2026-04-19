b4d371b72e785d6ac7ed72c51ccb53118cb9c653
Jeanpierre <jhonatan15021995@egmail.com>
2026-04-19 17:11:48 -0500

    limpieza de imports

--- ney/src/main/java/com/citas/ney/security/SecurityConfig.java
+++ ney/src/main/java/com/citas/ney/security/SecurityConfig.java
@@ -3,9 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.citas.ney.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 /*import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
